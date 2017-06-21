a7294139c8c3ae88739b07be0de4b35b02b4dbcd
Francisco Javier Bolívar Lupiáñez <fboli94@gmail.com>
2017-06-21 11:16:17 +0200

    Step 12

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,7 +12,7 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(title: params[:article][:title], body: params[:article][:body])
+    @article = Article.new(article_params)
     if @article.save
       redirect_to @article
     else
@@ -28,5 +28,11 @@ class ArticlesController < ApplicationController
 
   def update
 
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
   end
 end
