0f4a990b010db34afff587985e1ef0c076d3f01d
Francisco Javier Bolívar Lupiáñez <fboli94@gmail.com>
2017-06-22 12:33:14 +0200

    Step 36

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     resources :comments, only: [:create, :destroy, :update]
   end
   devise_for :users
-  root 'welcome#index'
+  root 'articles#index'
   get '/dashboard', to: 'welcome#dashboard'
   put '/articles/:id/publish', to: 'articles#publish'
   put '/articles/:id/unpublish', to: 'articles#unpublish'
